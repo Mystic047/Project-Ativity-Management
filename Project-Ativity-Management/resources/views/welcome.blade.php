1355eccfd317a5eb2af114c5c4324466163667a4
Kingsomsri45 <640112418053@bru.ac.th>
2024-03-14 19:24:30 +0700

    Update welcome.blade.php

--- Project-Ativity-Management/resources/views/welcome.blade.php
+++ Project-Ativity-Management/resources/views/welcome.blade.php
@@ -18,83 +18,85 @@
             border-radius: 10px;
             /* กำหนดรูปร่างของขอบ */
         }
-
     </style>
 </head>
+@extends('layout.master')
+@section('content')
 
-<body>
-    <div class="container">
-        <div class="row mt-4">
-            <div class="container">
-                <div class="carousel-background"> <!-- เพิ่ม div สำหรับกล่องพื้นหลัง -->
-                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="https://science.bru.ac.th/wp-content/uploads/2022/10/slider-1-1-copy.jpg"
-                                    class="d-block w-100" alt="...">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="https://science.bru.ac.th/wp-content/uploads/2022/10/slider-1-1-copy.jpg"
-                                    class="d-block w-100" alt="...">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="https://science.bru.ac.th/wp-content/uploads/2022/10/slider-1-1-copy.jpg"
-                                    class="d-block w-100" alt="...">
+    <body>
+        <div class="container">
+            <div class="row mt-4">
+                <div class="container">
+                    <div class="carousel-background"> <!-- เพิ่ม div สำหรับกล่องพื้นหลัง -->
+                        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="https://science.bru.ac.th/wp-content/uploads/2022/10/slider-1-1-copy.jpg"
+                                        class="d-block w-100" alt="...">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="https://science.bru.ac.th/wp-content/uploads/2022/10/slider-1-1-copy.jpg"
+                                        class="d-block w-100" alt="...">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="https://science.bru.ac.th/wp-content/uploads/2022/10/slider-1-1-copy.jpg"
+                                        class="d-block w-100" alt="...">
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                <!-- เพิ่มการ์ดสมัครเข้าร่วมกิจกรรมออนไลน์ -->
-                <div class="row mt-4">
-                    <div class="col">
-                        <div class="card w-100 bg-white border-1"> <!-- เพิ่มคลาส bg-light เพื่อให้การ์ดมีสีพื้นหลังเป็นสีสว่าง -->
-                            <div class="card-header">
-                                สมัครเข้าร่วมกิจกรรมออนไลน์
-                            </div>
-                            <div class="card-body">
-                                <table class="table table-bordered table-hover mb-4">
-                                    <thead>
-                                        <tr>
-                                            <th class="text-center">ข้อมูล</th>
-                                            <th>ชื่อกิจกรรม</th>
-                                            <th>การรับสมัคร</th>
+                    <!-- เพิ่มการ์ดสมัครเข้าร่วมกิจกรรมออนไลน์ -->
+                    <div class="row mt-4">
+                        <div class="col">
+                            <div class="card w-100 bg-white border-1">
+                                <!-- เพิ่มคลาส bg-light เพื่อให้การ์ดมีสีพื้นหลังเป็นสีสว่าง -->
+                                <div class="card-header">
+                                    สมัครเข้าร่วมกิจกรรมออนไลน์
+                                </div>
+                                <div class="card-body">
+                                    <table class="table table-bordered table-hover mb-4">
+                                        <thead>
+                                            <tr>
+                                                <th class="text-center">ข้อมูล</th>
+                                                <th>ชื่อกิจกรรม</th>
+                                                <th>การรับสมัคร</th>
 
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <!-- เพิ่มข้อมูลในตารางตามความเหมาะสม -->
-                                        <tr>
-                                            <td>สถานะเปิดปิด</td>
-                                            <td>ชื่อกิจ
-                                                <br>จัดโดย .... สถานที่ ......
-                                            </td>
-                                            <td>รับสมัคร
-                                                <br>วันที่ ....  จำนวนที่รับ ......
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>กิจกรรมที่ 2</td>
-                                            <td>ไม่รับสมัคร</td>
-                                            <td>ไม่รับสมัคร</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <!-- เพิ่มข้อมูลในตารางตามความเหมาะสม -->
+                                            <tr>
+                                                <td>สถานะเปิดปิด</td>
+                                                <td>ชื่อกิจ
+                                                    <br>จัดโดย .... สถานที่ ......
+                                                </td>
+                                                <td>รับสมัคร
+                                                    <br>วันที่ .... จำนวนที่รับ ......
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>กิจกรรมที่ 2</td>
+                                                <td>ไม่รับสมัคร</td>
+                                                <td>ไม่รับสมัคร</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-</body>
+    </body>
 
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
-</script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
-    integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous">
-</script>
-
-</html>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+        integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous">
+    </script>
+@endsection
+    </html>
