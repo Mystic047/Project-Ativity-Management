f65bc338e96509061e0b921a2638e738a866699e
Kingsomsri45 <640112418053@bru.ac.th>
2024-03-15 00:39:52 +0700

    Update welcome.blade.php

--- Project-Ativity-Management/resources/views/welcome.blade.php
+++ Project-Ativity-Management/resources/views/welcome.blade.php
@@ -7,11 +7,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai&display=swap" rel="stylesheet">
     <style>
         .carousel-background {
             background-color: #f0f0f0;
             padding: 20px;
             border-radius: 10px;
+        }
+        *{
+            font-family: 'Noto Sans Thai', sans-serif;
         }
     </style>
 </head>
