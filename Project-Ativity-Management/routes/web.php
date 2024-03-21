83405e1698a030d8190a7f1e65a0af1fedfc28d6
Kingsomsri45 <640112418053@bru.ac.th>
2024-03-21 22:16:39 +0700

    Update web.php

--- Project-Ativity-Management/routes/web.php
+++ Project-Ativity-Management/routes/web.php
@@ -19,3 +19,6 @@ Route::get('/', function () {
 Route::get('/profile', function () {
     return view('profile');
 });
+Route::get('/filedowload', function () {
+    return view('filedowload');
+});
