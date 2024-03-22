6ac880c6038ca92e334d13f7d2058384740befa0
MYSE4 <640112418047@bru.ac.th>
2024-03-22 11:47:35 +0700

    create profressor

--- Project-Ativity-Management/database/migrations/2024_03_14_170133_create_studens_table.php
+++ Project-Ativity-Management/database/migrations/2024_03_14_170133_create_studens_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up(): void   
     {
         Schema::create('studens', function (Blueprint $table) {
             $table->string('studens_id')->primary();
