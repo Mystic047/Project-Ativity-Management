a51e5531dae1d3d0f772506e3e0a96fd5be75303
Kingsomsri45 <640112418053@bru.ac.th>
2024-03-22 13:53:07 +0700

    Create login.blade.php

---
+++ Project-Ativity-Management/resources/views/login.blade.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+</head>
+  <body>
+    <!-- Login 8 - Bootstrap Brain Component -->
+<section class="bg-light p-3 p-md-4 p-xl-5">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-12 col-xxl-11">
+          <div class="card border-light-subtle shadow-sm">
+            <div class="row g-0">
+              <div class="col-12 col-md-6">
+                <img class="img-fluid rounded-start w-100 h-100 object-fit-cover" loading="lazy" src="https://scontent.fnak3-1.fna.fbcdn.net/v/t31.18172-8/21950074_1269279859866997_8640836755073465690_o.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFpppKCnHQ6apQI_uKg_TmFuMw2Ee_0M0e4zDYR7_QzR_m6s5E1RcX3qT5KLom9ewcBbppoDaELIO_IAkejl1bW&_nc_ohc=4nCwuyPaAz8AX_qJtyI&_nc_ht=scontent.fnak3-1.fna&oh=00_AfAeyJwD7BzKI7rvqAkuy4dEYV_WqbyCygi2ke5ooJ5D5Q&oe=662490C4" alt="Welcome back you've been missed!">
+              </div>
+              <div class="col-12 col-md-6 d-flex align-items-center justify-content-center">
+                <div class="col-12 col-lg-11 col-xl-10">
+                  <div class="card-body p-3 p-md-4 p-xl-5">
+                    <div class="row">
+                      <div class="col-12">
+                        <div class="mb-5">
+                          <div class="text-center mb-4">
+                            <a href="#!">
+                              <img src="https://science.bru.ac.th/wp-content/uploads/2018/03/sci-logo-1.jpg" alt="BootstrapBrain Logo" width="175" height="57">
+                            </a>
+                          </div>
+                          <h4 class="text-center">Welcome back you've been missed!</h4>
+                        </div>
+                      </div>
+                    </div>
+                    <form action="#!">
+                      <div class="row gy-3 overflow-hidden">
+                        <div class="col-12">
+                          <div class="form-floating mb-3">
+                            <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com" required>
+                            <label for="email" class="form-label">Email</label>
+                          </div>
+                        </div>
+                        <div class="col-12">
+                          <div class="form-floating mb-3">
+                            <input type="password" class="form-control" name="password" id="password" value="" placeholder="Password" required>
+                            <label for="password" class="form-label">Password</label>
+                          </div>
+                        </div>
+                        <div class="col-12">
+                          <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" name="remember_me" id="remember_me">
+                            <label class="form-check-label text-secondary" for="remember_me">
+                              Keep me logged in
+                            </label>
+                          </div>
+                        </div>
+                        <div class="col-12">
+                          <div class="d-grid">
+                            <button class="btn btn-dark btn-lg" type="submit">Log in now</button>
+                          </div>
+                        </div>
+                      </div>
+                    </form>
+                    <div class="row">
+                      <div class="col-12">
+                        <div class="d-flex gap-2 gap-md-4 flex-column flex-md-row justify-content-md-center mt-5">
+                          <a href="#!" class="link-secondary text-decoration-none">Create new account</a>
+                          <a href="#!" class="link-secondary text-decoration-none">Forgot password</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
