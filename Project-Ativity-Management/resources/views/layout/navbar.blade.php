3119a76eff36e2b10f03c42b6926174dc58532fd
Kingsomsri45 <640112418053@bru.ac.th>
2024-04-10 21:03:42 +0700

    fix : navbar

--- Project-Ativity-Management/resources/views/layout/navbar.blade.php
+++ Project-Ativity-Management/resources/views/layout/navbar.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Navbar with Logo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <style>
         /* เพิ่มขนาดภาพให้มีขนาดพอดีกับ Navbar */
@@ -137,7 +136,6 @@
         </div>
     </nav>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
